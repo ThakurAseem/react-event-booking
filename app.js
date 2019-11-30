b2863c5acdb7375389c17d83dafbc3218812f57b
Your Name <aseemthakur654@gmail.com>
2019-11-30 12:29:04 -0800

    i am stuck in graphql code

--- app.js
+++ app.js
@@ -2,59 +2,82 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/event');
 
 const app = express();
 
-const events=[];
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
-
-app.use('/graphql',graphqlHttp({
+app.use(
+  '/graphql',
+  graphqlHttp({
     schema: buildSchema(`
-       type Event {
-           _id: ID!
-           title: String!
-           description: String!
-           price: Float!
-           date: String!
-       }
-       input EventInput{
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-       }
-       type RootQuery{
-           events: [Event!]!
-       }
-       type RootMutation{
-           createEvent(eventInput: EventInput): Event
-       }
-       schema {
-           query: RootQuery
-           mutation: RootMutation
-       }
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        type RootQuery {
+            events: [Event!]!
+        }
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
     `),
     rootValue: {
-        events: () =>{
-            return events;
-        },
-        createEvent: (args) =>{
-            const event = {
-            _id:Math.random().toString(),
-            title : args.eventInput.title,
-            description:args.eventInput.description,
-            price:+args.eventInput.price,
-            date:args.eventInput.date
-            }
-            events.push(event);
-            return event;
+      events: async () => {
+        try {
+          const events = await Event.find();
+          return events.map(event => {
+            return { ...event._doc, _id: event.id };
+          });
         }
+        catch (err) {
+          throw err;
+        }
+      },
+      createEvent: async args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        try {
+          const result = await event.save();
+          console.log(result);
+          return { ...result };
+        }
+        catch (err) {
+          console.log(err);
+          throw err;
+        }
+      }
     },
     graphiql: true
-}))
+  })
+);
 
-app.listen(3000,()=>{
-    console.log("Running port on localhost 3000...");
-});
-
+mongoose
+  .connect('mongodb+srv://ABC:1234@cluster0-yszlp.mongodb.net/test',{ useNewUrlParser: true },{ useUnifiedTopology: true })
+  .then('on',()=>{
+      app.listen(3000,()=>{
+          console.log("This is running..");
+      });
+  })
+  .catch('once',(err)=>{
+      console.log(err);
+  })
